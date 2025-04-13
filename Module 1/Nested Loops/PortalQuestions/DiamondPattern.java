f33ecc4162617ac04f2cfd5ae941c16777f7d8ed
Kunal Jindal <kunaljindal@mnnit.ac.in>
2025-04-13 09:28:45 +0530

    Remove extra code

--- Module 1/Nested Loops/PortalQuestions/DiamondPattern.java
+++ Module 1/Nested Loops/PortalQuestions/DiamondPattern.java
@@ -5,35 +5,33 @@ import java.util.Scanner;
 public class DiamondPattern {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-      //  int t = sc.nextInt();
-       // while (t > 0) {
-            // main logic
-            int n = sc.nextInt();
+        int t = sc.nextInt();
 
+        for (int mt = 0; mt < t; mt++) {
+            int n = sc.nextInt();
             // upper half
-        int rows = n/2;
-            for(int i=1;i<=rows;i++) {
-                for(int j=1;j<=(n-i);j++) {
-                    System.out.print(" ");
+            int rows = n / 2+1;
+            for (int i = 1; i <= rows; i++) {
+                for (int j = 1; j <= (rows - i); j++) {
+                    System.out.print("  ");
                 }
-                for(int j=1;j<=(2*i-1);j++) {
+                for (int j = 1; j <= (2 * i - 1); j++) {
                     System.out.print("* ");
                 }
                 System.out.println();
             }
 
             // lower half
-        int m = n-rows;
-        for(int i=1;i<=m;i++) {
-            for(int j=1;j<=i;j++) {
-                System.out.print(" ");
+            int m = n - rows;
+            for (int i = m; i >=1 ; i--) {
+                for (int j = 1; j <= (m - i+1); j++) {
+                    System.out.print("  ");
+                }
+                for (int j = 1; j <= (2 * i - 1); j++) {
+                    System.out.print("* ");
+                }
+                System.out.println();
             }
-            for(int j=1;j<=(2*m-2*i+1);j++) {
-                System.out.print(" *");
-            }
-            System.out.println();
         }
-            //t--;
-        //}
     }
 }
