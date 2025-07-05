fbeb6ec15c3b396e29706bb5267333fe1a6f8942
Kunal Jindal <kunaljindal@mnnit.ac.in>
2025-07-05 13:30:59 +0530

    Linked list 4 and 5

--- Module 2/Linked List - 4/MergeSort/MergeSort.java
+++ Module 2/Linked List - 4/MergeSort/MergeSort.java
@@ -1,4 +1,105 @@
-package MergeSort;
+import java.util.*;
 
+class Solution {
+
+    static Node findMid(Node head) {
+        if(head==null || head.next==null) return head;
+
+        Node slow = head;
+        Node fast = head;
+
+        while(fast.next!=null && fast.next.next!=null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+
+        return slow;
+    }
+
+    static Node merge(Node head1, Node head2) {
+        Node curr1 = head1;
+        Node curr2 = head2;
+        Node dummy = new Node(-1);
+        Node currDummy = dummy;
+
+        while(curr1!=null && curr2!=null) {
+            if(curr1.data < curr2.data) {
+                currDummy.next = curr1;
+                curr1 = curr1.next;
+            }
+            else {
+                currDummy.next = curr2;
+                curr2 = curr2.next;
+            }
+
+            currDummy = currDummy.next;
+        }
+
+        if(curr1!=null) {
+            currDummy.next = curr1;
+        }
+
+        if(curr2!=null) {
+            currDummy.next = curr2;
+        }
+
+        return dummy.next;
+    }
+
+    public static Node mergesort(Node head){
+        if(head==null || head.next==null) return head;
+
+        // find mid
+        Node mid = findMid(head);
+        Node head1 = head;  // head of left list
+        Node head2 = mid.next;  // head of right list
+        mid.next = null;
+
+        head1 = mergesort(head1);
+        head2 = mergesort(head2);
+
+        Node newHead = merge(head1,head2);
+        return newHead;
+    }
+}
+
+
+class Node{
+    int data;
+    Node next = null;
+    Node(int data){
+        this.data = data;
+        next = null;
+    }
+}
+class LinkedList{
+    Node head;
+    void add(int data){
+        Node new_node = new Node(data);
+        if(head == null){
+            head = new_node;
+            return;
+        }
+        Node curr = head;
+        while(curr.next != null)
+            curr = curr.next;
+        curr.next = new_node;
+    }
+}
 public class MergeSort {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        LinkedList a = new LinkedList();
+        for(int i = 0; i < n; i++){
+            a.add(input.nextInt());
+        }
+        Solution Obj = new Solution();
+        a.head = Obj.mergesort(a.head);
+        Node h = a.head;
+        while(h != null){
+            System.out.print(h.data + " ");
+            h = h.next;
+        }
+    }
 }
---
+++ Module 2/Linked List - 5/Problems/BrowserHistory.java
@@ -0,0 +1,59 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Throwable {
+        Scanner sc = new Scanner(System.in);
+        String homepage = sc.nextLine();
+        int n = sc.nextInt();
+        sc.nextLine();
+        BrowserHistory browserHistory = new BrowserHistory(homepage);
+        for(int i = 0; i < n; i++) {
+            String q = sc.nextLine();
+            String[] query = q.split("\\s+");
+            if(query[0].equals("visit")){
+                browserHistory.visit(query[1]);
+            } else if(query[0].equals("back")){
+                browserHistory.back(Integer.parseInt(query[1]));
+            } else if(query[0].equals("forward")){
+                browserHistory.forward(Integer.parseInt(query[1]));
+            }
+        }
+    }
+}
+
+class Node {
+    String url;
+    Node next;
+    Node prev;
+
+    public Node(String url) {
+        this.url = url;
+    }
+}
+
+class BrowserHistory {
+    Node curr;
+    public BrowserHistory(String homepage) {
+        curr = new Node(homepage);
+    }
+    public void visit(String url) {
+        Node newNode = new Node(url);
+        this.curr.next = newNode;
+        newNode.prev = this.curr;
+        this.curr = newNode;
+    }
+    public void back(int steps) {
+        while(curr.prev!=null && steps > 0) {
+            this.curr = this.curr.prev;
+            steps--;
+        }
+        System.out.println(curr.url);
+    }
+    public void forward(int steps) {
+        while(curr.next!=null && steps > 0) {
+            this.curr = this.curr.next;
+            steps--;
+        }
+        System.out.println(curr.url);
+    }
+}
